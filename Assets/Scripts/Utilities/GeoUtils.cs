39488cbfed9b82225178f06f27066e97044ae28f
Jeremy Legere <jeremy.m.legere@gmail.com>
2025-06-11 14:25:13 -0300

    Refactor Haversine for clarity

--- Assets/Scripts/Utilities/GeoUtils.cs
+++ Assets/Scripts/Utilities/GeoUtils.cs
@@ -6,23 +6,22 @@ namespace GeoApp.Utilities
     public static class GeoUtils
     {
         const double EarthRadius = 6371000; // meters
+        const double Deg2Rad = PI / 180d;
 
         public static double Haversine(Vector2d a, Vector2d b)
         {
-            var dLat = ToRadians(b.x - a.x);
-            var dLon = ToRadians(b.y - a.y);
+            var dLat = (b.x - a.x) * Deg2Rad;
+            var dLon = (b.y - a.y) * Deg2Rad;
 
-            var lat1 = ToRadians(a.x);
-            var lat2 = ToRadians(b.x);
+            var lat1 = a.x * Deg2Rad;
+            var lat2 = b.x * Deg2Rad;
 
-            var sinDLat = Sin(dLat / 2);
-            var sinDLon = Sin(dLon / 2);
+            var sinDLat = Sin(dLat * 0.5);
+            var sinDLon = Sin(dLon * 0.5);
 
             var aa = sinDLat * sinDLat + Cos(lat1) * Cos(lat2) * sinDLon * sinDLon;
             var c = 2 * Atan2(Sqrt(aa), Sqrt(1 - aa));
             return EarthRadius * c;
         }
-
-        static double ToRadians(double degrees) => degrees * PI / 180d;
     }
 }
