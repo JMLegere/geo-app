0495d648af4214becfed685455dd71d09918ecc4
Jeremy Legere <jeremy.m.legere@gmail.com>
2025-06-15 13:26:37 -0300

    Fix Vector2d reference

--- Assets/Scripts/Domain/VoronoiCell.cs
+++ Assets/Scripts/Domain/VoronoiCell.cs
@@ -2,12 +2,17 @@ using System.Collections.Generic;
 
 namespace GeoApp.Domain
 {
-    public enum CellState { Unrevealed, Shadowed, Revealed }
+    public enum CellState
+    {
+        Unrevealed,
+        Shadowed,
+        Revealed
+    }
 
     public class VoronoiCell
     {
         public int index;
-        public List<Vector2d> polygon = new List<Vector2d>();
+        public List<Vector2d> polygon { get; } = new();
         public CellState state = CellState.Unrevealed;
     }
 }
